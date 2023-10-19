656b403f512a88a1b3ab7911a027b2011e9182c2
Arkady Wey <arkady.wey@maths.ox.ac.uk>
2023-10-19 16:38:48 +0100

    Add code towq submodule

--- template_package/template_subpackage/submodule.py
+++ template_package/template_subpackage/submodule.py
@@ -1,3 +1,12 @@
 """
 Module in subpackage.
 """
+import matplotlib
+from matplotlib import pyplot as plt
+import numpy
+
+def plot_data(data):
+    x_axis = numpy.linspace(start=1,stop=5,step=5)
+    y_axis = data
+    plt.plot(x_axis,y_axis)
+    plt.show()
