930b7e91deeca9eaa61a72439770baac7ba27b8b
Arkady Wey <arkady.wey@maths.ox.ac.uk>
2023-10-19 16:47:25 +0100

    Add code to run

--- template_package/run.py
+++ template_package/run.py
@@ -1,3 +1,9 @@
 """"
 Run code from package and subpackage.
 """
+import template_package
+import template_package.template_subpackage
+
+data = template_package.module.get_data()
+
+template_package.template_subpackage.submodule.plpot_data(data)
